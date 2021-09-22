6d266a0bbfaca5ea7a9611f0f4993606bb0292a3
Daelon02 <Daelon02@github.com>
2021-09-22 12:14:09 +0300

    Add hashing

---
+++ Makefile
@@ -0,0 +1,28 @@
+# Checks two given strings for equality.
+eq = $(if $(or $(1),$(2)),$(and $(findstring $(1),$(2)),\
+                                $(findstring $(2),$(1))),1)
+
+check:
+	SKIP_WASM_BUILD=1 cargo check
+
+test:
+	SKIP_WASM_BUILD=1 cargo test --all -- --nocapture --test-threads 1
+
+run:
+	SKIP_WASM_BUILD=1 cargo run --release
+
+build:
+	SKIP_WASM_BUILD=1 cargo build --release
+
+# Format Rust sources with rustfmt.
+#
+# Usage:
+#	make fmt [check=(no|yes)]
+
+fmt:
+	SKIP_WASM_BUILD=1 cargo +nightly fmt --all $(if $(call eq,$(check),yes),-- --check,)
+
+lint:
+	SKIP_WASM_BUILD=1 cargo clippy --workspace -- -D clippy::pedantic -D warnings
+
+.PHONY: lint fmt build run test check
--- hashing/src/lib.rs
+++ hashing/src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use aes::Aes256;
+use block_modes::{block_padding::Pkcs7, BlockMode, Cbc};
+
+type Aes256Cbc = Cbc<Aes256, Pkcs7>;
+
+pub fn encrypted_mnemonic(mnemonic: String, key: &str, iv: &str) -> Vec<u8> {
+    let target = mnemonic;
+
+    let key = hex!(key);
+    let iv = hex!(iv);
+    let cipher = Aes256Cbc::new_from_slices(&key, &iv).unwrap();
+
+    let ciphertext = cipher.encrypt_vec(target.as_bytes());
+    ciphertext
+}
+
+pub fn decrypted_mnemonic(key: String, iv: String, mnemonic: Vec<u8>) -> String {
+    let key = hex!(key);
+    let iv = hex!(iv);
+
+    // re-create cipher mode instance and decrypt the message
+    let cipher = Aes256Cbc::new_from_slices(&key, &iv).unwrap();
+    let mut buf = mnemonic.to_vec();
+    let decrypted_ciphertext = cipher.decrypt(&mut buf).unwrap();
+    let mnemonic_string =
+String::from_utf8(decrypted_ciphertext.to_vec()).unwrap();
+    mnemonic_string
 }
