42655ae0f6b0c1635886b8f999b6fc7018a72a6c
Daelon02 <Daelon02@github.com>
2021-09-23 12:05:56 +0300

    Update logger

--- request/src/lib.rs
+++ request/src/lib.rs
@@ -1,33 +1,35 @@
 use primitives::Error;
-use runtime::Call;
 use db::Database;
 
 use serde_json::Value;
 
 pub trait Request {
-    fn new(json: Value) -> Result<Self, Error>
+    /// Create new request object from json
+    ///
+    /// # Errors
+    /// If can't deserialize, return serde_json error
+    fn new(json: &Value) -> Result<Self, Error>
         where Self: Sized;
 
+    /// Return unique request id
+    /// uuid v4 as String
     fn get_id(&self) -> String;
 }
 
-pub trait BlockchainRequest {
-    fn get_call(&self) -> Result<Call, Error>;
-}
-
 pub trait OrchestratorRequest {
+    /// Return topic by which publish
+    /// response for this request into nats
+    fn get_topic(&self) -> String;
+
     /// Convert request, replace user_id by account_id
     ///
     /// # Errors
     /// Database errors
-    fn transform(&self, db: &Database) -> Result<Value, Error>;
+    fn process(&self, db: &Database) -> Result<Value, Error>;
 }
 
-pub trait Sendable {
-    fn get_topic(&self) -> String;
-
-    fn get_json(&self) -> Result<Value, Error>
-        where Self: serde::ser::Serialize {
-        serde_json::to_value(self).map_err(Error::SerdeJSON)
-    }
+pub trait Gettable {
+    /// Return topic by which
+    /// getting this request from nats
+    fn topic() -> String;
 }
