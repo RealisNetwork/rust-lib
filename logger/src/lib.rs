5485c3fa3bea0274b16365f64e1b91f6ad4e3aa4
Daelon02 <Daelon02@github.com>
2021-09-23 10:34:14 +0300

    Update logger

--- logger/src/lib.rs
+++ logger/src/lib.rs
@@ -21,8 +21,8 @@ pub fn new<W1, W2>(w_out: W1, w_err: W2) -> Logger
         W1: io::Write + Send + 'static,
         W2: io::Write + Send + 'static,
 {
-    // let drain_out = Json::new(w_out).build();
-    // let drain_err = Json::new(w_err).build();
+    let drain_out = Json::new(w_out).build();
+    let drain_err = Json::new(w_err).build();
 
     let drain_out = CompactFormat::new(PlainSyncDecorator::new(w_out)).build();
     let drain_err = CompactFormat::new(PlainSyncDecorator::new(w_err)).build();
@@ -104,28 +104,28 @@ pub fn logger_init() {
         .try_init();
 }
 
-// #[must_use]
-// pub fn term_new() -> Logger {
-//     let decorator = slog_term::TermDecorator::new().build();
-//     let drain = slog_term::CompactFormat::new(decorator).build().fuse();
-//     let drain = slog_async::Async::new(drain).build().fuse();
-//     let drain = drain.filter_level(Level::Info).fuse();
+#[must_use]
+pub fn term_new() -> Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+    let drain = drain.filter_level(Level::Info).fuse();
 
-//     let logger = slog::Logger::root(drain, o!());
+    let logger = slog::Logger::root(drain, o!());
 
-//     logger.new(o!(
-//         "msg" => PushFnValue(move |record : &Record, ser| {
-//             ser.emit(record.msg())
-//         }),
-//         "fqn" => PushFnValue(move |record : &Record, ser| {
-//              ser.emit(format_args!("{:?}, {}:{}", chrono::Utc::now(),
-//                 record.module(), record.line()))
-//         }),
-//         "time" => PushFnValue(move |_ : &Record, ser| {
-//             ser.emit(Local::now().to_rfc3339())
-//         }),
-//         "lvl" => FnValue(move |rinfo : &Record| {
-//             rinfo.level().as_str()
-//         }),
-//     ))
-// }
+    logger.new(o!(
+        "msg" => PushFnValue(move |record : &Record, ser| {
+            ser.emit(record.msg())
+        }),
+        "fqn" => PushFnValue(move |record : &Record, ser| {
+             ser.emit(format_args!("{:?}, {}:{}", chrono::Utc::now(),
+                record.module(), record.line()))
+        }),
+        "time" => PushFnValue(move |_ : &Record, ser| {
+            ser.emit(Local::now().to_rfc3339())
+        }),
+        "lvl" => FnValue(move |rinfo : &Record| {
+            rinfo.level().as_str()
+        }),
+    ))
+}
