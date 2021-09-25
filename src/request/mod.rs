c993ace5467d68d2f03f8e5ef216f7fe15ba7b75
HolyGrease <kireevdanil2000@gmail.com>
2021-09-25 16:18:51 +0300

    feat: update request traits

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,32 +1,18 @@
-use crate::db::Database;
-
-use crate::primitives::adapter::Error;
 use serde_json::Value;
 
 pub trait Request {
+    type Error;
     /// Create new request object from json
     ///
     /// # Errors
     /// If can't deserialize, return serde_json error
-    fn new(json: &Value) -> Result<Self, Error>
+    fn new(json: &Value) -> Result<Self, Self::Error>
     where
         Self: Sized;
 
     /// Return unique request id
     /// uuid v4 as String
     fn get_id(&self) -> String;
-}
-
-pub trait OrchestratorRequest {
-    /// Return topic by which publish
-    /// response for this request into nats
-    fn get_topic(&self) -> String;
-
-    /// Convert request, replace user_id by account_id
-    ///
-    /// # Errors
-    /// Database errors
-    fn process(&self, db: &Database) -> Result<Value, Error>;
 }
 
 pub trait Gettable {
