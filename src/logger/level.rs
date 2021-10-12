6b45e782b5f75d025c1e26f482effaa87d8d8e84
HolyGrease <kireevdanil2000@gmail.com>
2021-10-12 16:42:13 +0300

    fix: different levels support

--- src/logger/level.rs
+++ src/logger/level.rs
@@ -6,11 +6,11 @@ impl Parser {
     pub fn from_str(level: &str) -> Option<LevelFilter> {
         match level.to_lowercase().as_str() {
             "off" => Some(LevelFilter::Off),
-            "error" => Some(LevelFilter::Off),
-            "warn" => Some(LevelFilter::Off),
-            "info" => Some(LevelFilter::Off),
-            "debug" => Some(LevelFilter::Off),
-            "trace" => Some(LevelFilter::Off),
+            "error" => Some(LevelFilter::Error),
+            "warn" => Some(LevelFilter::Warn),
+            "info" => Some(LevelFilter::Info),
+            "debug" => Some(LevelFilter::Debug),
+            "trace" => Some(LevelFilter::Trace),
             _ => None
         }
     }
