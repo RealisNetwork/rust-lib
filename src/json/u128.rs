6799b0ddf8f1053bee03c7ee22a9200aec58a7b9
HolyGrease <kireevdanil2000@gmail.com>
2021-10-20 11:56:38 +0300

    feat: impl deserialize with move precision

--- src/json/u128.rs
+++ src/json/u128.rs
@@ -1,4 +1,5 @@
 use serde::{Serializer, Deserializer, Deserialize};
+use serde::de::Error;
 
 const DECIMALS: u8 = 12;
 
@@ -7,15 +8,37 @@ pub fn u128_from_string<'de, D>(deserializer: D) -> Result<u128, D::Error>
     where
         D: Deserializer<'de>,
 {
-    // f64 max value = 17 976 931 348 623 156.0
-    match String::deserialize(deserializer)?.parse::<f64>().map(|value| {
-        (value * (10_u64.pow(DECIMALS.into()) as f64)).round() as u128
-    }) {
-        Ok(value) => Ok(value),
-        Err(error) => Err(serde::de::Error::custom(format!(
-            "Cannot convert to f64 with error: {:?}",
-            error
-        ))),
+    let number = String::deserialize(deserializer)?;
+    if number.contains('.') {
+        let splitted = number.split('.').map(String::from).collect::<Vec<String>>();
+        let integer = splitted
+            .first()
+            .ok_or(Error::custom(String::from("Missing integer part!")))?
+            .parse::<u128>()
+            .map_err(|error|
+                Error::custom(format!("Cannot convert integer part to u128: {:?}", error)))?;
+        let fractional_length = splitted
+            .get(1)
+            .ok_or(Error::custom(String::from("Missing fractional part!")))?
+            .len();
+        let fractional = splitted
+            .get(1)
+            .ok_or(Error::custom(String::from("Missing fractional part!")))?
+            .parse::<u128>()
+            .map_err(|error|
+                Error::custom(format!("Cannot convert fractional part to u128: {:?}", error)))?;
+
+        let integer = integer * 10_u128.pow(DECIMALS.into());
+        let fractional = fractional * 10_u128.pow(DECIMALS as u32 - fractional_length as u32);
+
+        Ok(integer + fractional)
+    } else {
+        // If number is integer
+        let number = number
+            .parse::<u128>()
+            .map_err(|error| Error::custom(format!("Cannot convert to u128 with error: {}", error)))?;
+        Ok(
+            number * 10_u128.pow(DECIMALS.into()))
     }
 }
 
