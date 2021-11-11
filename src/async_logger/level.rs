46cd5bf6883a3f70366995187a17accf7d90a6bd
artem <lartem.1205@gmail.com>
2021-11-11 14:31:26 +0200

    init: async logger

---
+++ src/async_logger/level.rs
@@ -0,0 +1,17 @@
+use log::LevelFilter;
+
+pub struct Parser {}
+
+impl Parser {
+    pub fn from_str(level: &str) -> Option<LevelFilter> {
+        match level.to_lowercase().as_str() {
+            "off" => Some(LevelFilter::Off),
+            "error" => Some(LevelFilter::Error),
+            "warn" => Some(LevelFilter::Warn),
+            "info" => Some(LevelFilter::Info),
+            "debug" => Some(LevelFilter::Debug),
+            "trace" => Some(LevelFilter::Trace),
+            _ => None,
+        }
+    }
+}
