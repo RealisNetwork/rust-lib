de6334cd12de69e425288f78dcca7b839ecc8171
Daelon02 <gubin0202@gmail.com>
2021-11-26 15:54:28 +0200

    Add features

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
+#[cfg(feature = "async_logger")]
 pub mod async_logger;
 pub mod blockchain;
+#[cfg(feature = "config")]
 pub mod config;
+#[cfg(feature = "healthchecker")]
 pub mod healthchecker;
+#[cfg(feature = "ser/des-types")]
 pub mod json;
+#[cfg(feature = "logger")]
 pub mod logger;
+#[cfg(feature = "nats")]
 pub mod nats;
 pub mod primitives;
+#[cfg(feature = "vault")]
 pub mod vault;
