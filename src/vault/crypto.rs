c3781181e56eea165d2cef4855d523549dc38e33
HolyGrease <kireevdanil2000@gmail.com>
2021-11-29 16:57:42 +0200

    refactor: remove hardcoded keys

--- src/vault/crypto.rs
+++ src/vault/crypto.rs
@@ -1,26 +1,18 @@
-use aes::Aes256;
 use block_modes::{block_padding::Pkcs7, BlockMode, Cbc};
-use hex_literal::hex;
+use aes::Aes256;
 
 type Aes256Cbc = Cbc<Aes256, Pkcs7>;
 
-pub fn encrypted_mnemonic(mnemonic: &str, _key: &str, _iv: &str) -> Vec<u8> {
+pub fn encrypted_mnemonic(mnemonic: &str, key: &[u8], iv: &[u8]) -> Vec<u8> {
     let target = mnemonic;
 
-    let key = hex!("1cde61f165f5867d3de6ed6554770680df3b47d0d5ca5537c379569124ab4bec");
-    let iv = hex!("f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff");
     let cipher = Aes256Cbc::new_from_slices(&key, &iv).unwrap();
 
     let ciphertext = cipher.encrypt_vec(target.as_bytes());
     ciphertext
 }
 
-pub fn decrypted_mnemonic(_key: String, _iv: String, mnemonic: Vec<u8>) -> String {
-    // let key_format = format!("{:?}", key);
-    let key = hex!("1cde61f165f5867d3de6ed6554770680df3b47d0d5ca5537c379569124ab4bec");
-    let iv = hex!("f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff");
-
-    // re-create cipher mode instance and decrypt the message
+pub fn decrypted_mnemonic(key: &[u8], iv: &[u8], mnemonic: Vec<u8>) -> String {
     let cipher = Aes256Cbc::new_from_slices(&key, &iv).unwrap();
     let mut buf = mnemonic.to_vec();
     let decrypted_ciphertext = cipher.decrypt(&mut buf).unwrap();
