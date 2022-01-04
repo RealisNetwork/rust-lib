08ef5ff47a7449689ed67ee756c50c3952c7ac17
artem <lartem.1205@gmail.com>
2022-01-04 10:30:04 +0200

    add: fu build for json

--- src/nats/response_builder.rs
+++ src/nats/response_builder.rs
@@ -4,6 +4,22 @@ use serde_json::{json, Value};
 pub struct ResponseBuilder {}
 
 impl ResponseBuilder {
+
+    #[must_use]
+    pub fn build<T, P>(request: &T, response: &P) -> Value
+        where
+            T: Serialize,
+            P: Serialize,
+    {
+        json!({
+            "result": {
+                "request": request,
+                "response": response,
+            }
+        })
+    }
+
+    #[must_use]
     pub fn right<T, P>(request: T, value: P) -> Value
     where
         T: Serialize,
@@ -20,6 +36,7 @@ impl ResponseBuilder {
         })
     }
 
+    #[must_use]
     pub fn left<T, P>(request: T, msg: P) -> Value
     where
         T: Serialize,
