7ca0ac3cbfe0d274b11d88694034b796502b51b5
HolyGrease <kireevdanil2000@gmail.com>
2022-01-12 13:42:11 +0200

    feat: use prev lib for tls

--- src/inner_db/client_inner_builder.rs
+++ src/inner_db/client_inner_builder.rs
@@ -1,5 +1,6 @@
 use deadpool_postgres::{Config, Runtime, SslMode};
 use openssl::ssl::{SslConnector, SslMethod, SslVerifyMode};
+use postgres_openssl::MakeTlsConnector;
 use tokio_postgres::NoTls;
 use crate::inner_db::client_inner::DatabaseClientInner;
 
@@ -24,30 +25,31 @@ impl DatabaseClientInnerBuilder {
         let pool = if ssl {
             cfg.ssl_mode = Some(SslMode::Require);
 
-            // let mut builder = SslConnector::builder(SslMethod::tls_client()).unwrap();
-            // builder.set_verify(SslVerifyMode::NONE);
-            // builder.set_mode(openssl::ssl::SslMode::AUTO_RETRY);
+            let mut builder = SslConnector::builder(SslMethod::tls_client()).unwrap();
+            builder.set_verify(SslVerifyMode::NONE);
+            builder.set_mode(openssl::ssl::SslMode::AUTO_RETRY);
+            let tls = MakeTlsConnector::new(builder.build());
 
-            let mut root_store = rustls::RootCertStore::empty();
-            root_store.add_server_trust_anchors(
-                webpki_roots::TLS_SERVER_ROOTS
-                    .0
-                    .iter()
-                    .map(|ta| {
-                        rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
-                            ta.subject,
-                            ta.spki,
-                            ta.name_constraints,
-                        )
-                    })
-            );
+            // let mut root_store = rustls::RootCertStore::empty();
+            // root_store.add_server_trust_anchors(
+            //     webpki_roots::TLS_SERVER_ROOTS
+            //         .0
+            //         .iter()
+            //         .map(|ta| {
+            //             rustls::OwnedTrustAnchor::from_subject_spki_name_constraints(
+            //                 ta.subject,
+            //                 ta.spki,
+            //                 ta.name_constraints,
+            //             )
+            //         })
+            // );
+            //
+            // let config = rustls::ClientConfig::builder()
+            //     .with_safe_defaults()
+            //     .with_root_certificates(root_store)
+            //     .with_no_client_auth();
 
-            let config = rustls::ClientConfig::builder()
-                .with_safe_defaults()
-                .with_root_certificates(root_store)
-                .with_no_client_auth();
-
-            let tls = tokio_postgres_rustls::MakeRustlsConnect::new(config);
+            // let tls = tokio_postgres_rustls::MakeRustlsConnect::new(config);
 
             cfg.create_pool(Some(Runtime::Tokio1), tls) // TODO handle this unwrap
         } else {
