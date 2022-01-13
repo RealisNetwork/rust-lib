04ccb205115d2d93ad26676aff480febf46b5cd6
kazinarka <illia.fedotov@nure.ua>
2022-01-13 17:48:32 +0200

    add macros for retry

--- src/inner_db/client_inner.rs
+++ src/inner_db/client_inner.rs
@@ -42,9 +42,7 @@ impl DatabaseClientInner {
 
     pub fn get_backoff(&self) -> ExponentialBackoff {
         ExponentialBackoffBuilder::new()
-            .with_max_interval(Duration::from_secs(self.max_interval))
-            .with_randomization_factor(0.0)
-            .with_max_elapsed_time(Option::from(Duration::from_millis(5000)))
+            .with_max_elapsed_time(Option::from(Duration::from_millis(self.max_interval)))
             .build()
     }
 }
