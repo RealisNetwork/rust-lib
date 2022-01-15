848fbfbf6965ec80e750fa9a41645166844a1110
HolyGrease <kireevdanil2000@gmail.com>
2022-01-15 16:04:27 +0200

    feat: package types

---
+++ types/src/requests.rs
@@ -0,0 +1,37 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Response<T, Y> {
+    pub result: ResponseResult<T, Y>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResponseResult<T, Y> {
+    pub request: T,
+    pub response: ResponseResult<Y>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type")]
+pub enum ResponseResult<T> {
+    Left { value: ResponseError },
+    Right { value: T },
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ResponseError {
+    pub msg: String,
+    #[serde(rename = "type")]
+    pub error_type: String,
+    pub trace: Option<String>,
+    pub data: Option<Value>,
+    pub status: Option<i32>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct AuthInfo {
+    #[serde(rename = "userId")]
+    pub user_id: String,
+    pub address: Option<String>,
+    pub continent: Option<String>,
+}
