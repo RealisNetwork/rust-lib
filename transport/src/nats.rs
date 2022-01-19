8b9af7c0fbfd77992485ada2700edb6b51263904
HolyGrease <kireevdanil2000@gmail.com>
2022-01-19 09:40:39 +0200

    feat: minor changes

--- transport/src/nats.rs
+++ transport/src/nats.rs
@@ -1,13 +1,9 @@
 use crate::traits::Transport;
 use async_trait::async_trait;
-use ratsio::{RatsioError, StanClient, StanMessage, StanOptions, StanSid};
+use ratsio::{StanClient, StanOptions};
 use rust_lib::error_registry::{Nats as NatsError, RealisErrors};
-use std::{future::Future, io::Read, sync::Arc};
-use tokio::{
-    select,
-    sync::mpsc::{channel, Receiver},
-    time::error::Elapsed,
-};
+use std::sync::Arc;
+use futures::{Stream, StreamExt};
 
 #[derive(Clone)]
 pub struct Nats {
@@ -17,7 +13,7 @@ pub struct Nats {
 impl Nats {
     pub async fn new(nats_opts: &str, client_id: &str, cluster_id: &str) -> Self {
         let opts = StanOptions::with_options(nats_opts, cluster_id, &client_id[..]);
-        let stan_client = StanClient::from_options(opts).await.unwrap();
+        let stan_client = StanClient::from_options(opts).await.unwrap(); // TODO handle this unwrap
         Self { stan_client }
     }
 }
@@ -27,33 +23,16 @@ impl Transport for Nats {
     type Error = RealisErrors;
     type Message = Vec<u8>;
 
-    async fn publish(&self, topic: &str, message: Self::Message, topic_res: Option<String>) -> Result<(), Self::Error> {
-        match self.stan_client.publish(topic, &message).await {
-            Ok(()) => Ok(()),
-            Err(error) => Err(RealisErrors::Nats(NatsError::MessageReplyTimeout)),
-        }
+    async fn publish(&self, topic: &str, message: Self::Message, _topic_res: Option<String>) -> Result<(), Self::Error> {
+        self.stan_client.publish(topic, &message).await
+            .map_err(|_| RealisErrors::Nats(NatsError::Send))
     }
 
-    async fn subscribe(&self, topic: &str) -> Result<Receiver<Self::Message>, Self::Error> {
-        let (tx, rx) = channel(1024);
-        tokio::spawn(async move {
-            loop {
-                let (stan_id, mut stream) = self.stan_client.subscribe(topic, None, None).await?;
-                if let Some(raw_message) = stream.next() {
-                    tx.send(raw_message).await;
-                }
-            }
-        });
-        Ok(rx)
-    }
+    // TODO add unsubscribe by some way (callback?)
+    async fn subscribe(&self, topic: String) -> Result<Box<dyn Stream<Item = Self::Message>>, Self::Error> {
+        let (_, stream) = self.stan_client.subscribe(topic, None, None).await
+            .map_err(|_| RealisErrors::Nats(NatsError::Disconnected))?;
 
-    async fn get_one(&self, topic: &str) -> Result<Self::Message, Self::Error> {
-        let (stan_id, mut stream) = self.stan_client.subscribe(topic, None, None).await?;
-        let message = stream.next().await;
-        self.stan_client
-            .un_subscribe(&stan_id)
-            .await
-            .map_err(RealisErrors::Nats(NatsError::Disconnected))?;
-        Ok(message)
+        Ok(Box::new(stream.map(|message| message.payload)))
     }
 }
--- transport/src/traits.rs
+++ transport/src/traits.rs
@@ -1,28 +1,34 @@
 use async_trait::async_trait;
-use tokio::{
-    sync::mpsc::Receiver,
-    time::{error::Elapsed, timeout, Duration},
-};
+use tokio::time::{error::Elapsed, timeout, Duration};
+use futures::{Stream, StreamExt, Future};
 
 #[async_trait]
 pub trait Transport {
     type Message: Send;
     type Error: From<Elapsed>;
+    type SubscribeId;
 
     async fn publish(&self, topic: &str, message: Self::Message, topic_res: Option<String>) -> Result<(), Self::Error>;
 
-    async fn subscribe(&self, topic: &str) -> Result<Receiver<Self::Message>, Self::Error>;
+    async fn subscribe(&self, topic: String) -> Result<(Self::SubscribeId, Box<dyn Future<Output=dyn Stream<Item = Self::Message>>>), Self::Error>;
 
-    async fn get_one(&self, topic: &str) -> Result<Self::Message, Self::Error>;
+    async fn unsubscribe(&self, subscribe_id: Self::SubscribeId) -> Result<(), Self::Error>;
+
+    async fn get_one(&self, topic: String) -> Result<Option<Self::Message>, Self::Error> {
+        let (subscribe_id, stream) = self.subscribe(topic).await?;
+        let message = stream.next().await;
+        self.unsubscribe(subscribe_id).await?;
+        Ok(message)
+    }
 
     async fn message_reply(
         &self,
-        topic: &str,
-        topic_res: &str,
+        topic: String,
+        topic_res: String,
         message: Self::Message,
         duration: Option<Duration>,
-    ) -> Result<Self::Message, Self::Error> {
-        self.publish(topic, message, Some(topic_res.to_string())).await?;
+    ) -> Result<Option<Self::Message>, Self::Error> {
+        self.publish(&topic, message, Some(topic_res.to_string())).await?;
         match duration {
             Some(duration) => timeout(duration, self.get_one(topic_res)).await?,
             None => timeout(Duration::from_secs(25), self.get_one(topic_res)).await?,
