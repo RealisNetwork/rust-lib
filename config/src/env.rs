e20d33d9ae40827fd82a3ff7c3e7ab71767090f2
HolyGrease <kireevdanil2000@gmail.com>
2022-01-20 10:33:39 +0200

    feat: rewrite env config loader

---
+++ config/src/env.rs
@@ -0,0 +1,44 @@
+use dotenv::Error;
+
+pub enum EnvLoadedError {
+    Load(Error),
+    Convert(String)
+}
+
+impl From<Error> for EnvLoadedError {
+    fn from(error: Error) -> Self {
+        Self::Load(error)
+    }
+}
+
+impl From<String> for EnvLoadedError {
+    fn from(error_msg: String) -> Self {
+        Self::Convert(error_msg)
+    }
+}
+
+pub trait EnvLoaded: Sized {
+    fn load(key: &str) -> Result<Self, EnvLoadedError>;
+}
+
+impl EnvLoaded for bool {
+    fn load(key: &str) -> Result<Self, EnvLoadedError> {
+        Ok(dotenv::var(key)?
+            .parse::<bool>()
+            .map_err(|error| error.to_string())?)
+    }
+}
+
+impl EnvLoaded for u16 {
+    fn load(key: &str) -> Result<Self, EnvLoadedError> {
+        Ok(dotenv::var(key)?
+            .parse::<u16>()
+            .map_err(|error| error.to_string())?)
+    }
+}
+
+impl EnvLoaded for String {
+    fn load(key: &str) -> Result<Self, EnvLoadedError> {
+        Ok(dotenv::var(key)?)
+    }
+}
