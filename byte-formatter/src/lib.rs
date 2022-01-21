410c9ba8b978e6f35aa7aca9467a654c4858a814
HolyGrease <kireevdanil2000@gmail.com>
2022-01-21 13:34:20 +0200

    feat: simplify imports

--- byte-formatter/src/lib.rs
+++ byte-formatter/src/lib.rs
@@ -4,6 +4,7 @@ pub mod errors;
 pub mod ser;
 pub mod de;
 
+pub use realis_macros::{ByteSerialize, ByteDeserialize};
 pub use byte_reader::ByteReader;
 pub use byte_writer::ByteWriter;
 pub use ser::ByteSerialize;
