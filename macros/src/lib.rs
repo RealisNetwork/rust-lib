042ae563ee7e9fc30eec92b474db72ed0ccaa05e
kazinarka <illia.fedotov@nure.ua>
2022-01-22 11:19:45 +0200

    add macros for env

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,7 +1,26 @@
+mod symbol;
+
+use quote::{ToTokens, quote};
+use crate::symbol::parse_lit_into_expr_path;
+use std::default::Default;
 use convert_case::{Case, Casing};
 use proc_macro::TokenStream;
-use quote::quote;
-use syn::{self, parse_macro_input, Data, DeriveInput, FnArg, Ident, ItemFn, TypeTuple, ItemStruct, __private::Span};
+use syn::{
+    self,
+    parse_macro_input,
+    Data, DeriveInput,
+    FnArg,
+    Ident,
+    ItemFn,
+    TypeTuple,
+    ItemStruct,
+    __private::Span,
+    Meta::{NameValue, Path},
+    NestedMeta::Meta,
+    Lit::Str,
+    Type,
+    Lit,
+};
 
 /// # Panics
 #[proc_macro_derive(Gettable, attributes(gettable))]
@@ -173,4 +192,101 @@ pub fn byte_decode_macro_derive(item: TokenStream) -> TokenStream {
         }
     };
     gen.into()
+}
+
+#[proc_macro_derive(Env, attributes(env))]
+pub fn config_macro_derive(item: TokenStream) -> TokenStream {
+    let item_struct = syn::parse::<ItemStruct>(item).unwrap();
+    let struct_name = item_struct.ident;
+
+    let fields = item_struct.fields
+        .into_iter()
+        .map(|field| {
+            let field_name = field.ident.unwrap().to_token_stream();
+
+            let mut field_attributes = Field::default();
+
+            field
+                .attrs
+                .into_iter()
+                .map(|attr| symbol::get_env_meta_items(&attr))
+                .flatten()
+                .for_each(|meta| {
+                    match meta {
+                        Meta(NameValue(m)) if m.path == symbol::RENAME => field_attributes.field_type = EnvRenameAttrs::Rename(m.lit),
+                        Meta(Path(path)) if path == symbol::FLATTEN => field_attributes.field_type = EnvRenameAttrs::Flatten(field_name.to_string()),
+                        Meta(NameValue(m)) if m.path == symbol::RENAME_ABS => field_attributes.field_type = EnvRenameAttrs::RenameAbs(m.lit),
+                        Meta(Path(path)) if path == symbol::DEFAULT => field_attributes.default_type = EnvDefaultAttrs::Default,
+                        Meta(NameValue(m)) if m.path == symbol::DEFAULT_PATH => field_attributes.default_type = EnvDefaultAttrs::DefaultPath(m.lit),
+                        _ => {},
+                    }
+                });
+
+            let env_name = match field_attributes.field_type {
+                EnvRenameAttrs::Rename(Str(str)) => format!("{}_{}", struct_name.to_string(), str.value()).to_case(Case::UpperSnake),
+                EnvRenameAttrs::RenameAbs(Str(str)) => format!("{}", str.value()).to_case(Case::UpperSnake),
+                EnvRenameAttrs::Flatten(_) => format!("{}", field_name).to_case(Case::UpperSnake),
+                _ => format!("{}_{}", struct_name.to_string(), field_name.to_string()).to_case(Case::UpperSnake)
+            };
+
+            match (field_attributes.default_type, field.ty) {
+                (EnvDefaultAttrs::Default, Type::Path(field_type)) => {
+                    quote! {
+                        #field_name: #field_type::load(#env_name).unwrap_or_default(),
+                    }
+                },
+                (EnvDefaultAttrs::DefaultPath(lit), Type::Path(field_type)) => {
+                    let path = parse_lit_into_expr_path(&lit);
+                    quote! {
+                        #field_name: #field_type::load(#env_name).unwrap_or(#path()),
+                    }
+                },
+                (EnvDefaultAttrs::Empty, Type::Path(field_type)) => {
+                    quote! {
+                        #field_name: #field_type::load(#env_name)?,
+                    }
+                },
+                (_, field_type) => panic!("Env don't support this type: {:?}", field_type),
+            }
+        });
+
+    let code = quote! {
+        impl EnvLoaded for #struct_name {
+            fn load(_key: &str) -> Result<Self, EnvLoadedError> {
+                Ok(Self {
+                    #(#fields)*
+                })
+            }
+        }
+    };
+
+    code.into()
+}
+
+#[derive(PartialEq)]
+enum EnvRenameAttrs {
+    Flatten(String),
+    RenameAbs(Lit),
+    Rename(Lit),
+    Empty,
+}
+
+enum EnvDefaultAttrs {
+    Default,
+    DefaultPath(Lit),
+    Empty,
+}
+
+struct Field {
+    field_type: EnvRenameAttrs,
+    default_type: EnvDefaultAttrs,
+}
+
+impl Default for Field {
+    fn default() -> Self {
+        Self {
+            field_type: EnvRenameAttrs::Empty,
+            default_type: EnvDefaultAttrs::Empty,
+        }
+    }
 }
