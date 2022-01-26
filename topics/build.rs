7ca8911ca715e0282688f874d33ddc2c8a42eaaf
HolyGrease <kireevdanil2000@gmail.com>
2022-01-26 09:44:32 +0200

    feat: add rebuild feature

--- topics/build.rs
+++ topics/build.rs
@@ -4,20 +4,25 @@ use std::path::PathBuf;
 use config::env::EnvLoaded;
 
 fn main() {
-    // let filename = "topics.rs";
-    // let path = "./src";
+    #[cfg(feature = "rebuild")]
+    generate();
+}
 
-    // let mut code = TokenStream::default();
+fn generate() {
+    let filename = "topics.rs";
+    let path = "./src";
 
-    // let loader: GitLoader = EnvLoaded::load("").unwrap();
+    let mut code = TokenStream::default();
 
-    // loader
-    //     .load()
-    //     .unwrap()
-    //     .iter()
-    //     .for_each(|topic| code.extend(topic.to_token_stream()));
+    let loader: GitLoader = EnvLoaded::load("").unwrap();
 
-    // let out = &mut PathBuf::from(path);
-    // out.push(filename);
-    // std::fs::write(out, code.to_string()).unwrap();
+    loader
+        .load()
+        .unwrap()
+        .iter()
+        .for_each(|topic| code.extend(topic.to_token_stream()));
+
+    let out = &mut PathBuf::from(path);
+    out.push(filename);
+    std::fs::write(out, code.to_string()).unwrap();
 }
