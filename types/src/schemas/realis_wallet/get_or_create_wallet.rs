fd261c5b7876dc565d0b73400d405e3dcaa384a8
HolyGrease <kireevdanil2000@gmail.com>
2022-01-31 10:48:38 +0200

    update: create wallet schema

--- types/src/schemas/realis_wallet/get_or_create_wallet.rs
+++ types/src/schemas/realis_wallet/get_or_create_wallet.rs
@@ -4,6 +4,8 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct GetOrCreateWalletSchema {
     pub id: String,
+    #[serde(rename = "topicResponse")]
+    pub topic_res: String,
     #[serde(rename = "authInfo")]
     pub auth_info: AuthInfo,
 }
