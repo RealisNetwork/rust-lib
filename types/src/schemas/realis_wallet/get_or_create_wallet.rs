4fa95fcdd2be12dd2688a5f20026e25504a31e52
HolyGrease <kireevdanil2000@gmail.com>
2022-01-31 11:39:47 +0200

    feat: add topic res alias

--- types/src/schemas/realis_wallet/get_or_create_wallet.rs
+++ types/src/schemas/realis_wallet/get_or_create_wallet.rs
@@ -4,7 +4,7 @@ use serde::{Serialize, Deserialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct GetOrCreateWalletSchema {
     pub id: String,
-    #[serde(rename = "topicResponse")]
+    #[serde(rename = "topicResponse", alias = "topicRes")]
     pub topic_res: String,
     #[serde(rename = "authInfo")]
     pub auth_info: AuthInfo,
