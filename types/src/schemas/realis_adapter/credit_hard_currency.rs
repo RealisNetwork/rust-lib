e0b59fad6737ec8afc8d2185e650dc6016ef5e92
HolyGrease <kireevdanil2000@gmail.com>
2022-02-01 14:18:04 +0200

    feat: add serialize/deserialize

--- types/src/schemas/realis_adapter/credit_hard_currency.rs
+++ types/src/schemas/realis_adapter/credit_hard_currency.rs
@@ -1,12 +1,15 @@
 use runtime::AccountId;
 use rust_lib::json::u128::{u128_to_string, u128_from_string};
+use serde::{Serialize, Deserialize};
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CreditHardCurrencySchema {
     pub id: String,
     pub account_id: AccountId,
     pub params: CreditHardCurrencyParams
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CreditHardCurrencyParams {
     #[serde(serialize_with = "u128_to_string")]
     #[serde(deserialize_with = "u128_from_string")]
