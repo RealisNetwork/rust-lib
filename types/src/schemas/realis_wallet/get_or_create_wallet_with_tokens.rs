d9adc1e46f48e5ec51a01f75bb5842a9f9d033cc
artem <lartem.1205@gmail.com>
2022-02-01 11:33:27 +0200

    add: minor changes

--- types/src/schemas/realis_wallet/get_or_create_wallet_with_tokens.rs
+++ types/src/schemas/realis_wallet/get_or_create_wallet_with_tokens.rs
@@ -6,7 +6,14 @@ pub struct GetOrCreateWalletWithTokensSchema {
     pub id: String,
     #[serde(rename = "topicResponse", alias = "topicRes")]
     pub topic_res: String,
-    pub params: Params,
+    pub params: GetOrCreateWalletWithTokensParams,
     #[serde(rename = "authInfo")]
     pub auth_info: AuthInfo,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GetOrCreateWalletWithTokensParams {
+    #[serde(serialize_with = "u128_to_string")]
+    #[serde(deserialize_with = "u128_from_string")]
+    pub amount: Option<u128>
 }
