d2bcd990ff881a0b8e15fab75fcc7d4a9d1a8d91
artem <lartem.1205@gmail.com>
2022-02-03 12:18:39 +0200

    fix: minor changes

--- types/src/schemas/realis_wallet/get_or_create_wallet.rs
+++ types/src/schemas/realis_wallet/get_or_create_wallet.rs
@@ -13,7 +13,7 @@ pub struct GetOrCreateWalletSchema {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct CreateBinanceWalletSchema {
     pub id: String,
-    #[serde(rename = "topicResponse", alias = "topicRes")]
+    #[serde(rename = "topicRes", alias = "topicResponse")]
     pub topic_res: String,
     #[serde(rename = "userId")]
     pub user_id: String,
