87f100d58ad47d0df49810b10088d6046b342e53
HolyGrease <kireevdanil2000@gmail.com>
2022-02-07 12:31:43 +0200

    fix: access level

--- config/src/env/mod.rs
+++ config/src/env/mod.rs
@@ -2,8 +2,8 @@ pub mod env;
 #[cfg(feature = "nats")]
 pub mod nats;
 #[cfg(feature = "database")]
-mod database;
+pub mod database;
 #[cfg(feature = "healthchecker")]
-mod healthchecker;
+pub mod healthchecker;
 
 pub use env::{EnvLoaded, EnvLoadedError, Env};
