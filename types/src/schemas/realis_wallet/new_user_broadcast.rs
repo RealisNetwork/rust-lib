ad7f29c8d656d2bd7a835c99a4a5d93c19c7fddc
ilya <illia.fedotov1807@gmail.com>
2022-02-21 10:47:50 +0200

    add new_user_broadcast

--- types/src/schemas/realis_wallet/new_user_broadcast.rs
+++ types/src/schemas/realis_wallet/new_user_broadcast.rs
@@ -2,7 +2,6 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct NewUserBroadcastSchema {
-    pub id: String,
     #[serde(rename = "userId")]
     pub user_id: String,
 }
