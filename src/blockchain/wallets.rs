ea7260b759616de7641fbc561bb7f0cf62612cf5
Your Name <you@example.com>
2022-03-09 14:25:31 +0200

    feat: minor changes

--- src/blockchain/wallets.rs
+++ src/blockchain/wallets.rs
@@ -1,8 +1,7 @@
 use runtime::AccountId;
 
 use serde::Deserialize;
-use sp_core::Pair;
-use sp_runtime::app_crypto::sr25519;
+use substrate_api_client::{Pair, sp_runtime::app_crypto::sr25519};
 
 pub trait RealisWallet {
     fn get_public() -> AccountId;
