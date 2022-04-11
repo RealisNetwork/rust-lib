9f4100837440e5ca8e3b2b902f9392191f20c1cc
HolyGrease <kireevdanil2000@gmail.com>
2022-04-11 20:29:32 +0300

    feat: add TODO

--- types/src/schemas/realis_adapter/mod.rs
+++ types/src/schemas/realis_adapter/mod.rs
@@ -2,3 +2,7 @@ pub mod add_nft_item;
 pub mod credit_transfer;
 pub mod debit_transfer;
 pub mod remove_nft_item;
+
+// TODO: TransferTokensToPlayer
+// TODO: TransferNftToPlayer
+// TODO: ChangeBatch
