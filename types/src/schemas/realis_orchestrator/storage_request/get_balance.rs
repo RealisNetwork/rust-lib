e36781f249430e4e4a242c211521e3e4947124ba
BeginArtem <lartem.1205@gmail.com>
2022-04-26 13:27:43 +0300

    fix: topicResponse in struct

--- types/src/schemas/realis_orchestrator/storage_request/get_balance.rs
+++ types/src/schemas/realis_orchestrator/storage_request/get_balance.rs
@@ -5,7 +5,7 @@ use runtime::AccountId;
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct GetBalanceSchema {
     pub id: String,
-    #[serde(rename = "topicRes", alias = "topicResponse")]
+    #[serde(rename = "topicResponse")]
     pub topic_res: String,
     #[serde(rename = "accountId")]
     pub account_id: AccountId,
