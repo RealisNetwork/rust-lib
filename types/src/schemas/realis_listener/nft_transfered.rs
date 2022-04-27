0cfff98888cd84b11e7e4a2da07f7c69fe16d142
Ruslan <ruslan.tahiiev@swanteams.com>
2022-04-27 14:15:17 +0300

    add schemas for listener

---
+++ types/src/schemas/realis_listener/nft_transfered.rs
@@ -0,0 +1,27 @@
+use realis_primitives::TokenId;
+use rust_lib::json::u128::{u128_from_string, u128_to_string};
+use runtime::AccountId;
+use serde::{Deserialize, Serialize};
+use substrate_api_client::Hash;
+
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct NftTransferedSchema {
+    #[serde(rename = "topicResponse")]
+    pub topic_res: String,
+    pub id: String,
+    pub params: NftTransferedParams,
+}
+
+#[derive(Deserialize, Serialize, Clone, Debug)]
+pub struct NftTransferedParams {
+    pub from: AccountId,
+    pub to: AccountId,
+    #[serde(rename = "tokenId")]
+    pub token_id: TokenId,
+    #[serde(rename = "blockHash")]
+    pub block_hash: Hash,
+    #[serde(rename = "txHash")]
+    pub tx_hash: Hash,
+}
+
