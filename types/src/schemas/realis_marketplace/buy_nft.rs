5ab5baa131f78df9282b19552943169c8ef16406
Ruslan <ruslan.tahiiev@swanteams.com>
2022-04-27 16:30:24 +0300

    add schema realis-marketplace

---
+++ types/src/schemas/realis_marketplace/buy_nft.rs
@@ -0,0 +1,43 @@
+use crate::{
+    requests::AuthInfo,
+    schemas::realis_orchestrator::marketplace::buy_nft::BuyNftSchema as OrchestratorBuyNftSchema,
+};
+use realis_primitives::TokenId;
+use rust_lib::json::token_id::{token_id_from_string, token_id_to_string};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BuyNftSchema {
+    pub id: String,
+    #[serde(rename = "topicRes", alias = "topicResponse")]
+    pub topic_res: String,
+    pub method: String,
+    pub params: BuyNftParams,
+    #[serde(rename = "authInfo")]
+    pub auth_info: AuthInfo,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct BuyNftParams {
+    #[serde(serialize_with = "token_id_to_string")]
+    #[serde(deserialize_with = "token_id_from_string")]
+    #[serde(rename = "tokenId")]
+    pub token_id: TokenId,
+    #[serde(rename = "accountId")]
+    pub account_id: AccountId,
+}
+
+impl BuyNftSchema {
+    pub fn new(other: OrchestratorBuyNftSchema, account_id: AccountId) -> Self {
+        Self {
+            id: other.id,
+            topic_res: other.topic_res,
+            method: other.method,
+            auth_info: other.auth_info,
+            params: BuyNftParams {
+                token_id: other.params.token_id,
+                account_id,
+            },
+        }
+    }
+}
---
+++ types/src/schemas/realis_marketplace/sell_nft.rs
@@ -0,0 +1,51 @@
+use crate::{
+    requests::AuthInfo, 
+    Amount,
+    schemas::realis_orchestrator::marketplace::sell_nft::SellNftSchema as OrchestratorSellNftSchema,
+};
+use realis_primitives::TokenId;
+use rust_lib::json::{
+    token_id::{token_id_from_string, token_id_to_string},
+    u128::{u128_from_string, u128_to_string},
+};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SellNftSchema {
+    pub id: String,
+    #[serde(rename = "topicRes", alias = "topicResponse")]
+    pub topic_res: String,
+    pub method: String,
+    pub params: SellNftParams,
+    #[serde(rename = "authInfo")]
+    pub auth_info: AuthInfo,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct SellNftParams {
+    #[serde(serialize_with = "token_id_to_string")]
+    #[serde(deserialize_with = "token_id_from_string")]
+    #[serde(rename = "tokenId")]
+    pub token_id: TokenId,
+    #[serde(serialize_with = "u128_to_string")]
+    #[serde(deserialize_with = "u128_from_string")]
+    pub amount: Amount,
+    #[serde(rename = "accountId")]
+    pub account_id: AccountId,
+}
+
+impl SellNftSchema {
+    pub fn new(other: OrchestratorSellNftSchema, account_id: AccountId) -> Self {
+        Self {
+            id: other.id,
+            topic_res: other.topic_res,
+            method: other.method,
+            auth_info: other.auth_info,
+            params: SellNftParams {
+                token_id: other.params.token_id,
+                amount: other.params.amount,
+                account_id,
+            },
+        }
+    }
+}
