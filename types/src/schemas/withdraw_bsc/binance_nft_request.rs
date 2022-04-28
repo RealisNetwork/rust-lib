d2ea2a19e2dc56f05878350b61e7b3ee2813fea0
gleb protasov <protasovgleb290@gmail.com>
2022-04-28 12:56:01 +0300

    withdraw-bsc schemas

---
+++ types/src/schemas/withdraw_bsc/binance_nft_request.rs
@@ -0,0 +1,23 @@
+use runtime::AccountId;
+use serde::{Deserialize, Serialize};
+use rust_lib::json::token_id::{token_id_from_string,token_id_to_string};
+use realis_primitives::TokenId;
+
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct BinanceNftRequestSchema {
+    pub id: String,
+    pub params: BinanceNftSchemaParams,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct BinanceNftSchemaParams {
+    #[serde(rename = "accountId")]
+    pub account_id: String,
+    #[serde(rename = "tokenId")]
+    #[serde(serialize_with = "token_id_to_string")]
+    #[serde(deserialize_with = "token_id_from_string")]
+    pub token_id: TokenId,
+    #[serde(rename = "from")]
+    pub from_account_id: AccountId,
+}
---
+++ types/src/schemas/withdraw_bsc/binance_tokens_request.rs
@@ -0,0 +1,22 @@
+use rust_lib::json::u128::{u128_from_string,u128_to_string}
+use runtime::AccountId;
+use serde::{Deserialize, Serialize};
+use crate::Amount;
+
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct BinanceTokensSchema {
+    pub id: String,
+    pub params: BinanceTokensSchemaParams,
+}
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct BinanceTokensSchemaParam{
+    #[serde(rename = "accountId")]
+    pub account_id: String,
+    #[serde(serialize_with = "u128_to_string")]
+    #[serde(deserialize_with = "u128_from_string")]
+    pub amount: Amount,
+    #[serde(rename = "from")]
+    pub from_account_id: AccountId,
+}
+}
