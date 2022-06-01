73a8e73fbd13048f41d7fe1ab3514269063c1dcb
DmytryG <dmytrygolub@gmail.com>
2022-06-01 16:57:40 +0300

    Fix adapter schemas

--- types/src/schemas/realis_adapter/common.rs
+++ types/src/schemas/realis_adapter/common.rs
@@ -1,4 +1,4 @@
-use crate::requests::{Response, ResponseError, ResponseMessage, ResponseResult};
+use crate::requests::{Response, ResponseMessage, ResponseResult};
 use error_registry::BaseError;
 use serde::{Deserialize, Serialize};
 
@@ -11,7 +11,7 @@ pub struct BlockchainResponseParams {
 }
 
 impl BlockchainResponseParams {
-    pub fn into_response<T>(self, request: T) -> Response<T, BlockchainResponseParams> {
+    pub fn into_response<T>(self, request: T) -> Response<T, BlockchainResponseParams, ()> {
         Response {
             result: ResponseResult {
                 request,
@@ -20,7 +20,7 @@ impl BlockchainResponseParams {
         }
     }
 
-    pub fn into_left_response<T>(request: T, msg: String, error_type: BaseError<()>) -> Response<T, ()> {
+    pub fn into_left_response<T>(request: T, msg: String, error_type: BaseError<()>) -> Response<T, (), ()> {
         Response {
             result: ResponseResult {
                 request,
