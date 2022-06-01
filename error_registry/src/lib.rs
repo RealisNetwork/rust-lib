c99a779591cfd17d94b90364e54ef75d052b22f6
RTahiiev <ruslan.tahiiev@swanteams.com>
2022-06-01 09:28:43 +0300

    doc

--- error_registry/src/lib.rs
+++ error_registry/src/lib.rs
@@ -105,6 +105,12 @@ impl<D> Default for BaseError<D> {
     /// Use only if you explicitly want to get a default error!
     /// # Returns
     /// ```
+    /// use backtrace::Backtrace;
+    /// use error_registry::custom_errors::CustomErrorType;
+    /// use error_registry::ErrorType;
+    ///
+    /// let trace = Backtrace::new();
+    ///
     /// Self {
     ///     msg: String::from("Default error."),
     ///     error_type: ErrorType::Custom(CustomErrorType::Default),
@@ -212,7 +218,7 @@ mod tests {
         let error = ratsio::error::RatsioError::AckInboxMissing;
         assert_eq!(
             format!("{:?}", BaseError::<()>::from(error).error_type),
-            format!("{:?}", ErrorType::Custom(CustomErrorType::Nats(Nats::Send)))
+            format!("{:?}", ErrorType::Custom(CustomErrorType::Nats(CustomNats::Send)))
         )
     }
 
