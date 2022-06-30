044cf1af20f64d34d0276fa19b3d1e1015cebb2b
dmytryG <dmytrygolub@gmail.com>
2022-06-30 14:37:49 +0300

    Remove redundant code

--- config/src/env/mod.rs
+++ config/src/env/mod.rs
@@ -1,8 +1,4 @@
-// #[cfg(feature = "database")]
-// pub mod database;
 pub mod env;
 pub mod healthchecker;
-// #[cfg(feature = "nats")]
-// pub mod nats;
 
 pub use env::{Env, EnvLoaded};
--- transport/src/lib.rs
+++ transport/src/lib.rs
@@ -4,7 +4,7 @@ pub mod response;
 pub mod subscription;
 pub mod transport;
 
+pub use crate::transport::{stan::StanTransport, Transport, VTransport};
 pub use message::{ReceivedMessage, VReceivedMessage};
 pub use response::{stan::Response, VResponse};
 pub use subscription::{Subscription, VSubscription};
-pub use crate::transport::{stan::StanTransport, Transport, VTransport};
