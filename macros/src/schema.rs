7cf19b5ebc802021303d48a2fd84a527fe6343c7
RTahiiev <ruslan.tahiiev@swanteams.com>
2022-07-07 17:01:48 +0300

    Changes by glebprotasov rework baseerror, add macros schema

--- macros/src/schema.rs
+++ macros/src/schema.rs
@@ -3,7 +3,7 @@ use syn::{ItemFn, ItemStruct};
 use quote::quote;
 
 
-pub fn schema_macros(item: TokenStream)->TokenStream {
+pub fn schema_macros(item: TokenStream) -> TokenStream {
     let item_fn = syn::parse::<ItemStruct>(item).unwrap();
     let ident = item_fn.ident;
     let result = quote! {
