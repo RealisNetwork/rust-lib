40e1b002d126c3f9c8001f2d862381ae7b3f2525
dmytryG <dmytrygolub@gmail.com>
2022-07-15 14:09:37 +0300

    Lint

--- config/src/env/env.rs
+++ config/src/env/env.rs
@@ -8,49 +8,42 @@ pub trait EnvLoaded: Sized {
 
 impl EnvLoaded for bool {
     fn load(key: Option<String>) -> Result<Self, BaseError<()>> {
-        // println!("Read env by key: {:?}", key);
         Ok(dotenv::var(key.unwrap())?.parse::<bool>()?)
     }
 }
 
 impl EnvLoaded for usize {
     fn load(key: Option<String>) -> Result<Self, BaseError<()>> {
-        // println!("Read env by key: {:?}", key);
         Ok(dotenv::var(key.unwrap())?.parse::<usize>()?)
     }
 }
 
 impl EnvLoaded for u16 {
     fn load(key: Option<String>) -> Result<Self, BaseError<()>> {
-        // println!("Read env by key: {:?}", key);
         Ok(dotenv::var(key.unwrap())?.parse::<u16>()?)
     }
 }
 
 impl EnvLoaded for u32 {
     fn load(key: Option<String>) -> Result<Self, BaseError<()>> {
-        // println!("Read env by key: {:?}", key);
         Ok(dotenv::var(key.unwrap())?.parse::<u32>()?)
     }
 }
 
 impl EnvLoaded for u64 {
     fn load(key: Option<String>) -> Result<Self, BaseError<()>> {
-        // println!("Read env by key: {:?}", key);
         Ok(dotenv::var(key.unwrap())?.parse::<u64>()?)
     }
 }
 
 impl EnvLoaded for Vec<u8> {
     fn load(key: Option<String>) -> Result<Self, BaseError<()>> {
-        // println!("Read env by key: {:?}", key);
         Ok(hex::decode(dotenv::var(key.unwrap())?)?)
     }
 }
 
 impl EnvLoaded for String {
     fn load(key: Option<String>) -> Result<Self, BaseError<()>> {
-        // println!("Read env by key: {:?}", key);
         Ok(dotenv::var(key.unwrap())?)
     }
 }
