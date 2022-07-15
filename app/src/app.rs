335e187b322e3b1d29cee39e2ad38928911bd669
dmytryG <dmytrygolub@gmail.com>
2022-07-15 11:21:39 +0300

    Dependency conteiner while building App

--- app/src/app.rs
+++ app/src/app.rs
@@ -2,25 +2,42 @@ use async_trait::async_trait;
 use log::LevelFilter;
 use tokio::sync::Mutex;
 
+// pub trait DependencyContainable{};
+
 #[async_trait]
 pub trait Runnable: Send + Sync {
     async fn run(&mut self);
 }
 
-pub struct App {
+pub struct App<T> {
     services: Vec<Box<Mutex<dyn Runnable>>>,
+    dependency_container: T,
 }
 
-#[allow(clippy::derivable_impls)]
-impl Default for App {
-    fn default() -> Self {
-        Self { services: vec![] }
+// #[allow(clippy::derivable_impls)]
+// impl<T> Default for App<T> {
+//     fn default() -> Self {
+//         Self { services: vec![] }
+//     }
+// }
+
+
+
+impl<T: Clone + Send + Sync> App <T>{
+    pub fn new(dependency_container: T) -> Self {
+        Self {
+            services: vec![],
+            dependency_container,
+        }
     }
-}
 
-impl App {
     pub fn push(mut self, service: impl Runnable + 'static) -> Self {
         self.services.push(Box::new(Mutex::new(service)));
+        self
+    }
+
+    pub fn push_with_dependency<Service: 'static + Runnable + From<T>>(mut self) -> Self {
+        self.services.push(Box::new(Mutex::new(Service::from(self.dependency_container.clone()))));
         self
     }
 
@@ -34,10 +51,11 @@ impl App {
             .init();
         self
     }
+
 }
 
 #[async_trait]
-impl Runnable for App {
+impl<T: Clone + Send + Sync> Runnable for App<T> {
     async fn run(&mut self) {
         let services = self.services.drain(..);
         futures::future::join_all(services.into_iter().map(|service| {
