2a554185cb86575fa0846ba4f4f0668db57ac54b
BeginArtem <lartem.1205@gmail.com>
2022-07-20 09:39:09 +0300

    update dependencies

--- schemas/src/generated_schemas/binance_wallet/mod.rs
+++ schemas/src/generated_schemas/binance_wallet/mod.rs
@@ -1,3 +1,1 @@
-pub mod wallet_create_wallet;
-pub mod wallet_get_address_by_user_id;
-pub mod wallet_get_user_id_by_address;
+pub mod wallet_create_wallet ; pub mod wallet_get_address_by_user_id ; pub mod wallet_get_user_id_by_address ;
