0037dbb08a4b514c5c85e0ca50c167fe03a319e8
BeginArtem <lartem.1205@gmail.com>
2022-07-22 16:27:59 +0300

    add: dependencies to prelude

--- app/src/prelude.rs
+++ app/src/prelude.rs
@@ -1,12 +1,14 @@
 pub use crate::app::{App, DependencyContainerParameter, Runnable};
 pub use crate::broadcast_app::{BroadcastApp, BroadcastService};
+pub use crate::error_registry::{
+    custom_errors::CustomErrorType, generated_errors::GeneratedError, BaseError,
+};
 pub use crate::healthchecker::{
     Alivable, Healthchecker, HealthcheckerHTTPBuilder, HealthcheckerServer,
 };
-pub use crate::error_registry::{BaseError, custom_errors::Cus};
 pub use crate::service_app::{Service, ServiceApp};
 pub use crate::transport::{
     ReceivedMessage, Response, StanTransport, Subscription, Transport, VReceivedMessage, VResponse,
     VSubscription, VTransport,
 };
-pub use config::env::env_loaded::{EnvLoaded, Env};
+pub use config::env::env_loaded::{Env, EnvLoaded};
