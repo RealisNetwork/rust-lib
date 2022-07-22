6b3f6c6141b5fbd6c39b558959453c31bc8d75f4
BeginArtem <lartem.1205@gmail.com>
2022-07-22 17:57:25 +0300

    add: Errors to prelude

--- app/src/prelude.rs
+++ app/src/prelude.rs
@@ -1,7 +1,17 @@
 pub use crate::app::{App, DependencyContainerParameter, Runnable};
 pub use crate::broadcast_app::{BroadcastApp, BroadcastService};
 pub use crate::error_registry::{
-    custom_errors::CustomErrorType, generated_errors::GeneratedError, BaseError,
+    custom_errors::CustomErrorType::{
+        Blockchain as CustomBlockchain, Common as CustomCommon, Db as CustomDb, Default,
+        EnvLoadedError, Nats as CustomNats, Rpc, Utils as CustomUtils,
+    },
+    generated_errors::GeneratedError::{
+        Action, AdminOptions, Auth, Bff, Blockchain, Bytes, BytesFormatter, CatsAndDragons, Common,
+        Critical, Cron, Db, Fs, Functions, Geo, GooglePlay, Http, Nats, Orchestrator, Permissions,
+        ProductFactory, ProductRegistry, Profile, Promo, Redis, Referrals, RestorePassword, Roles,
+        Soul, Status, Transactions, TwoFactorAuth, Utils, Validation,
+    },
+    BaseError,
 };
 pub use crate::healthchecker::{
     Alivable, Healthchecker, HealthcheckerHTTPBuilder, HealthcheckerServer,
