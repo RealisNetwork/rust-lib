7efd6fea574abd14a80903b340fa2dc80c0de171
BeginArtem <lartem.1205@gmail.com>
2022-07-22 16:25:16 +0300

    add: BaseError to prelude

--- app/src/prelude.rs
+++ app/src/prelude.rs
@@ -3,6 +3,7 @@ pub use crate::broadcast_app::{BroadcastApp, BroadcastService};
 pub use crate::healthchecker::{
     Alivable, Healthchecker, HealthcheckerHTTPBuilder, HealthcheckerServer,
 };
+pub use crate::error_registry::{BaseError, custom_errors::Cus};
 pub use crate::service_app::{Service, ServiceApp};
 pub use crate::transport::{
     ReceivedMessage, Response, StanTransport, Subscription, Transport, VReceivedMessage, VResponse,
