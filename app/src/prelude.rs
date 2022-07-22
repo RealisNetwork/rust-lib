b8fe35a6beb4832c58f59d36f0ce14e7e6bbe52d
BeginArtem <lartem.1205@gmail.com>
2022-07-22 18:00:03 +0300

    minor changes

--- app/src/prelude.rs
+++ app/src/prelude.rs
@@ -1,15 +1,16 @@
 pub use crate::app::{App, DependencyContainerParameter, Runnable};
 pub use crate::broadcast_app::{BroadcastApp, BroadcastService};
 pub use crate::error_registry::{
-    custom_errors::CustomErrorType::{
-        Blockchain as CustomBlockchain, Common as CustomCommon, Db as CustomDb, Default,
-        EnvLoadedError, Nats as CustomNats, Rpc, Utils as CustomUtils,
+    custom_errors::{
+        Blockchain as CustomBlockchain, Common as CustomCommon, CustomErrorType, Db as CustomDb,
+        Default, EnvLoadedError, Nats as CustomNats, Rpc, Utils as CustomUtils,
     },
-    generated_errors::GeneratedError::{
+    generated_errors::{
         Action, AdminOptions, Auth, Bff, Blockchain, Bytes, BytesFormatter, CatsAndDragons, Common,
-        Critical, Cron, Db, Fs, Functions, Geo, GooglePlay, Http, Nats, Orchestrator, Permissions,
-        ProductFactory, ProductRegistry, Profile, Promo, Redis, Referrals, RestorePassword, Roles,
-        Soul, Status, Transactions, TwoFactorAuth, Utils, Validation,
+        Critical, Cron, Db, Fs, Functions, GeneratedError, Geo, GooglePlay, Http, Nats,
+        Orchestrator, Permissions, ProductFactory, ProductRegistry, Profile, Promo, Redis,
+        Referrals, RestorePassword, Roles, Soul, Status, Transactions, TwoFactorAuth, Utils,
+        Validation,
     },
     BaseError,
 };
