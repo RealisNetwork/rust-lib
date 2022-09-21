fa90d4ad141743cd0cd38cc3f387f78f0d9f7e05
BeginArtem <lartem.1205@gmail.com>
2022-09-21 09:42:42 +0300

    impl schema for value

--- schemas/src/lib.rs
+++ schemas/src/lib.rs
@@ -5,7 +5,7 @@ pub use crate::common::*;
 
 use serde::de::DeserializeOwned;
 use serde::Serialize;
-use serde_json::Value;
+use serde_json::{json, Value};
 use std::fmt::Debug;
 
 pub trait Schema: DeserializeOwned + Serialize + Clone + Debug + Send + Sync {
@@ -16,4 +16,10 @@ pub trait Agent: Schema {
     fn topic() -> &'static str;
     fn method() -> &'static str;
     fn agent() -> &'static str;
+}
+
+impl Schema for Value {
+    fn schema() -> Value {
+        json!({})
+    }
 }
